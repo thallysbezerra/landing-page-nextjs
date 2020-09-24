d159bfa16cf72d2540eebd0744399b12f6ac6e98
Thallys Bezerra <bezerra.thallys@gmail.com>
2020-09-24 00:35:39 -0300

    Making dynamics Header and SectionAboutProject

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,42 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      title
+      description
+      image {
         alternativeText
         url
       }
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        alternativeText
+        url
+      }
+    }
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...header
+      ...logo
+      ...sectionAboutProject
     }
   }
 `
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,32 @@
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
 export type LogoProps = {
   alternativeText: string
   url: string
 }
 
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
+  header: HeaderProps
   logo: LogoProps
+  sectionAboutProject: SectionAboutProjectProps
 }
